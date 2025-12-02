0ec548b1d863bfecc5eaa4a5c20a1d70e98a9d5a
dogz1lla <dogz1lla@t480arch.xyz>
2025-12-02 19:15:40 +0100

    adding a makefile, adding a compile_commands.json for clangd

--- compile_ws.sh
+++ compile_ws.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -Wall -Wextra -pedantic chess.c bitboard.c set_operations.c one_step.c lib/wsServer/src/ws.c lib/wsServer/src/utf8.c lib/wsServer/src/handshake.c lib/wsServer/src/sha1.c lib/wsServer/src/base64.c -Ilib/wsServer/src -Ilib/wsServer/include -o chess
+gcc -Wall -Wextra -pedantic ws_example.c lib/wsServer/src/ws.c lib/wsServer/src/utf8.c lib/wsServer/src/handshake.c lib/wsServer/src/sha1.c lib/wsServer/src/base64.c -Ilib/wsServer/src -Ilib/wsServer/include -o ws_example
---
+++ ws_example.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ws.h>
+
+/**
+ * @brief This function is called whenever a new connection is opened.
+ * @param client Client connection.
+ */
+void onopen(ws_cli_conn_t client)
+{
+    char *cli;
+    cli = ws_getaddress(client);
+    printf("Connection opened, addr: %s\n", cli);
+}
+
+/**
+ * @brief This function is called whenever a connection is closed.
+ * @param client Client connection.
+ */
+void onclose(ws_cli_conn_t client)
+{
+    char *cli;
+    cli = ws_getaddress(client);
+    printf("Connection closed, addr: %s\n", cli);
+}
+
+/**
+ * @brief Message events goes here.
+ * @param client Client connection.
+ * @param msg    Message content.
+ * @param size   Message size.
+ * @param type   Message type.
+ */
+void onmessage(ws_cli_conn_t client,
+    const unsigned char *msg, uint64_t size, int type)
+{
+    char *cli;
+    cli = ws_getaddress(client);
+    printf("I receive a message: %s (%zu), from: %s\n", msg,
+        size, cli);
+
+    sleep(2);
+    ws_sendframe_txt(client, "hello");
+    sleep(2);
+    ws_sendframe_txt(client, "world");
+}
+
+int main(void)
+{
+    /*
+     * Main loop, this function never* returns.
+     *
+     * *If the .thread_loop is != 0, a new thread is created
+     * to handle new connections and ws_socket() becomes
+     * non-blocking.
+     */
+    ws_socket(&(struct ws_server){
+        /*
+         * Bind host, such as:
+         * localhost -> localhost/127.0.0.1
+         * 0.0.0.0   -> global IPv4
+         * ::        -> global IPv4+IPv6 (Dual stack)
+         */
+        .host = "localhost",
+        .port = 8080,
+        .thread_loop   = 0,
+        .timeout_ms    = 1000,
+        .evs.onopen    = &onopen,
+        .evs.onclose   = &onclose,
+        .evs.onmessage = &onmessage
+    });
+
+    return (0);
+}
